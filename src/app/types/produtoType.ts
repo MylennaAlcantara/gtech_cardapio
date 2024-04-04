7ffbb8059f81f3c5c06f7da239ca12ae7827f5ce
Mylenna Alcantara Araujo <mylenna.a.a.corinthiana@gmail.com>
2024-04-04 16:40:54 -0300

    feat: adicionado propriedades id_grupo e nome_produto ao type

--- src/app/types/produtoType.ts
+++ src/app/types/produtoType.ts
@@ -2,6 +2,8 @@ export interface ProdutoType{
    id: number,
    id_filial: number,
    id_produto: number,
+   id_grupo: number,
+   nome_produto: string,
    descricao_produto: string,
    preco_venda: number,
    foto: Blob,
